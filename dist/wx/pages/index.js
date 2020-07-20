81ccfa2cbc06294711bb8485af7fe84c32b8049f
Sanzro <sanzrolee@gamil.com>
2020-07-20 20:21:21 +0800

    bottom tabbar switch different pages.

--- dist/wx/pages/index.js
+++ dist/wx/pages/index.js
@@ -2,9 +2,9 @@
 var window = window || {};
 
 window["webpackJsonp"] = require("../bundle.js");
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[2],{
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[4],{
 
-/***/ 204:
+/***/ 214:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -22,7 +22,7 @@ global.currentSrcMode = "wx"
     this._r();
   }
 };
-/* harmony import */ var _babel_loader_node_modules_mpxjs_webpack_plugin_lib_selector_type_script_index_0_index_mpx_isFirst_true__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(205);
+/* harmony import */ var _babel_loader_node_modules_mpxjs_webpack_plugin_lib_selector_type_script_index_0_index_mpx_isFirst_true__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(215);
 /* harmony import */ var _babel_loader_node_modules_mpxjs_webpack_plugin_lib_selector_type_script_index_0_index_mpx_isFirst_true__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_loader_node_modules_mpxjs_webpack_plugin_lib_selector_type_script_index_0_index_mpx_isFirst_true__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _babel_loader_node_modules_mpxjs_webpack_plugin_lib_selector_type_script_index_0_index_mpx_isFirst_true__WEBPACK_IMPORTED_MODULE_0__) if(__WEBPACK_IMPORT_KEY__ !== 'default') (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _babel_loader_node_modules_mpxjs_webpack_plugin_lib_selector_type_script_index_0_index_mpx_isFirst_true__WEBPACK_IMPORTED_MODULE_0__[key]; }) }(__WEBPACK_IMPORT_KEY__));
 global.currentModuleId
@@ -30,20 +30,20 @@ global.currentModuleId
 
 
 /* styles */
-__webpack_require__(206)
+__webpack_require__(216)
 
 /* json */
-__webpack_require__(207)
+__webpack_require__(217)
 
 /* template */
-__webpack_require__(208)
+__webpack_require__(218)
 
 
 /* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(1)))
 
 /***/ }),
 
-/***/ 205:
+/***/ 215:
 /***/ (function(module, exports) {
 
 
@@ -53,25 +53,25 @@ Page({
 
 /***/ }),
 
-/***/ 206:
+/***/ 216:
 /***/ (function(module, exports) {
 
 // removed by extractor
 
 /***/ }),
 
-/***/ 207:
+/***/ 217:
 /***/ (function(module, exports) {
 
 // removed by extractor
 
 /***/ }),
 
-/***/ 208:
+/***/ 218:
 /***/ (function(module, exports) {
 
 // removed by extractor
 
 /***/ })
 
-},[[204,1]]]);
+},[[214,1]]]);
