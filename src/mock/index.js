8cab6feb24c0dec4bf006ad59e526b10e747d413
Sanzro-Lee <sanzrolee@gmail.com>
2020-08-28 01:02:15 +0800

    ✨ Feat(weapp mock feature): mock feature, put all data in one file

---
+++ src/mock/index.js
@@ -0,0 +1,286 @@
+import mock from "@mpxjs/mock"
+
+export default () => mock([
+		{
+			url: "http://helperfaction.com/toptab/all",
+			rule: {
+				"all": [
+					{
+          title: '家电维修',
+          desc: '家里变压器坏了，快来人啊！',
+          name: '林小姐',
+          dist: '1 km',
+          price: 1000,
+          thumb: 'https://raw.githubusercontent.com/Sanzro-Lee/HELPER_FACTION_official_weapp_customer/05f0a55cfcb143435e535c4f1d2ea7185029e060/src/material/%E5%88%86%E7%B1%BB%E6%A0%87%E7%A4%BA%E5%9B%BE-01.svg'
+        },
+        {
+          title: '其它',
+          desc: '清洁房子。',
+          name: '王先生',
+          dist: '5 km',
+          price: 500,
+          thumb: 'https://raw.githubusercontent.com/Sanzro-Lee/HELPER_FACTION_official_weapp_customer/05f0a55cfcb143435e535c4f1d2ea7185029e060/src/material/%E5%88%86%E7%B1%BB%E6%A0%87%E7%A4%BA%E5%9B%BE-04.svg'
+        },
+        {
+          title: '居家维修',
+          desc: '想把发霉的墙推倒，重新砌起来。',
+          name: '刘先生',
+          dist: '10 km',
+          price: 2000,
+          thumb: 'https://raw.githubusercontent.com/Sanzro-Lee/HELPER_FACTION_official_weapp_customer/05f0a55cfcb143435e535c4f1d2ea7185029e060/src/material/%E5%88%86%E7%B1%BB%E6%A0%87%E7%A4%BA%E5%9B%BE-02.svg'
+        },
+        {
+          title: '货物搬迁',
+          desc: '刚找到新房子，要搬家了。',
+          name: '张先生',
+          dist: '4 km',
+          price: 1000,
+          thumb: 'https://raw.githubusercontent.com/Sanzro-Lee/HELPER_FACTION_official_weapp_customer/05f0a55cfcb143435e535c4f1d2ea7185029e060/src/material/%E5%88%86%E7%B1%BB%E6%A0%87%E7%A4%BA%E5%9B%BE-03.svg'
+        },
+        {
+          title: '家电维修',
+          desc: '家里变压器坏了，快来人啊！',
+          name: '林小姐',
+          dist: '3 km',
+          price: 1000,
+          thumb: 'https://raw.githubusercontent.com/Sanzro-Lee/HELPER_FACTION_official_weapp_customer/05f0a55cfcb143435e535c4f1d2ea7185029e060/src/material/%E5%88%86%E7%B1%BB%E6%A0%87%E7%A4%BA%E5%9B%BE-01.svg'
+        },
+        {
+          title: '其它',
+          desc: '清洁房子。',
+          name: '王先生',
+          dist: '2 km',
+          price: 500,
+          thumb: 'https://raw.githubusercontent.com/Sanzro-Lee/HELPER_FACTION_official_weapp_customer/05f0a55cfcb143435e535c4f1d2ea7185029e060/src/material/%E5%88%86%E7%B1%BB%E6%A0%87%E7%A4%BA%E5%9B%BE-04.svg'
+        },
+        {
+          title: '居家维修',
+          desc: '想把发霉的墙推倒，重新砌起来。',
+          name: '刘先生',
+          dist: '0.5 km',
+          price: 2000,
+          thumb: 'https://raw.githubusercontent.com/Sanzro-Lee/HELPER_FACTION_official_weapp_customer/05f0a55cfcb143435e535c4f1d2ea7185029e060/src/material/%E5%88%86%E7%B1%BB%E6%A0%87%E7%A4%BA%E5%9B%BE-02.svg'
+        },
+        {
+          title: '家电维修',
+          desc: '家里变压器坏了，快来人啊！',
+          name: '林小姐',
+          dist: '2.5 km',
+          price: 1000,
+          thumb: 'https://raw.githubusercontent.com/Sanzro-Lee/HELPER_FACTION_official_weapp_customer/05f0a55cfcb143435e535c4f1d2ea7185029e060/src/material/%E5%88%86%E7%B1%BB%E6%A0%87%E7%A4%BA%E5%9B%BE-01.svg'
+        }]
+			}
+		},
+		{
+			url: "http://helperfaction.com/toptab/homerepair",
+			rule: {
+				"homerepair": [
+					{
+						title: '居家维修',
+						desc: '想把发霉的墙推倒，重新砌起来。',
+						name: '刘先生',
+						dist: '0.5 km',
+						price: 2000,
+						thumb: 'https://raw.githubusercontent.com/Sanzro-Lee/HELPER_FACTION_official_weapp_customer/05f0a55cfcb143435e535c4f1d2ea7185029e060/src/material/%E5%88%86%E7%B1%BB%E6%A0%87%E7%A4%BA%E5%9B%BE-02.svg'
+					}
+				]
+			}
+		},
+		{
+			url: "http://helperfaction.com/toptab/appliancerepair",
+			rule: {
+				"appliancerepair": [
+					{
+						title: '家电维修',
+						desc: '家里变压器坏了，快来人啊！',
+						name: '林小姐',
+						dist: '4 km',
+						price: 1000,
+						thumb: 'https://raw.githubusercontent.com/Sanzro-Lee/HELPER_FACTION_official_weapp_customer/05f0a55cfcb143435e535c4f1d2ea7185029e060/src/material/%E5%88%86%E7%B1%BB%E6%A0%87%E7%A4%BA%E5%9B%BE-01.svg'
+					}
+				]
+			}
+		},
+		{
+			url: "http://helperfaction.com/toptab/goodsmove",
+			rule: {
+				"goodsmove": [
+					{
+						title: '货物搬迁',
+						desc: '刚找到新房子，要搬家了。',
+						name: '张先生',
+						dist: '3 km',
+						price: 1000,
+						thumb: 'https://raw.githubusercontent.com/Sanzro-Lee/HELPER_FACTION_official_weapp_customer/05f0a55cfcb143435e535c4f1d2ea7185029e060/src/material/%E5%88%86%E7%B1%BB%E6%A0%87%E7%A4%BA%E5%9B%BE-03.svg'
+					}
+				]
+			}
+		},
+		{
+			url: "http://helperfaction.com/toptab/others",
+			rule: {
+				"others": [
+					{
+						title: '其它',
+						desc: '清洁房子。',
+						name: '王先生',
+						dist: '2 km',
+						price: 500,
+						thumb: 'https://raw.githubusercontent.com/Sanzro-Lee/HELPER_FACTION_official_weapp_customer/05f0a55cfcb143435e535c4f1d2ea7185029e060/src/material/%E5%88%86%E7%B1%BB%E6%A0%87%E7%A4%BA%E5%9B%BE-04.svg'
+					}
+				]
+			}
+		},
+    {
+			url: "http://helperfaction.com/demand/list",
+			rule: {
+				"list": [
+					{
+						title: '家电维修',
+						desc: '家里变压器坏了，快来人啊！',
+						name: '林小姐',
+						dist: '1 km',
+						price: 1000,
+						thumb: 'https://raw.githubusercontent.com/Sanzro-Lee/HELPER_FACTION_official_weapp_customer/05f0a55cfcb143435e535c4f1d2ea7185029e060/src/material/%E5%88%86%E7%B1%BB%E6%A0%87%E7%A4%BA%E5%9B%BE-01.svg',
+						show: true,
+						statustagstyle: {
+							background: 'linear-gradient(to right, #5FD8A6, #A9DFA6)'
+						}
+					},
+					{
+						title: '其它',
+						desc: '清洁房子。',
+						name: '王先生',
+						dist: '5 km',
+						price: 500,
+						thumb: 'https://raw.githubusercontent.com/Sanzro-Lee/HELPER_FACTION_official_weapp_customer/05f0a55cfcb143435e535c4f1d2ea7185029e060/src/material/%E5%88%86%E7%B1%BB%E6%A0%87%E7%A4%BA%E5%9B%BE-04.svg',
+						show: true,
+						statustagstyle: {
+							background: 'linear-gradient(to right, #F8E255, #EBC354)'
+						}
+					},
+					{
+						title: '居家维修',
+						desc: '想把发霉的墙推倒，重新砌起来。',
+						name: '刘先生',
+						dist: '10 km',
+						price: 2000,
+						thumb: 'https://raw.githubusercontent.com/Sanzro-Lee/HELPER_FACTION_official_weapp_customer/05f0a55cfcb143435e535c4f1d2ea7185029e060/src/material/%E5%88%86%E7%B1%BB%E6%A0%87%E7%A4%BA%E5%9B%BE-02.svg',
+						show: true,
+						statustagstyle: {
+							background: 'linear-gradient(to right, #5FD8A6, #A9DFA6)'
+						}
+					},
+					{
+						title: '货物搬迁',
+						desc: '刚找到新房子，要搬家了。',
+						name: '张先生',
+						dist: '4 km',
+						price: 1000,
+						thumb: 'https://raw.githubusercontent.com/Sanzro-Lee/HELPER_FACTION_official_weapp_customer/05f0a55cfcb143435e535c4f1d2ea7185029e060/src/material/%E5%88%86%E7%B1%BB%E6%A0%87%E7%A4%BA%E5%9B%BE-03.svg',
+						show: true,
+						statustagstyle: {
+							background: 'linear-gradient(to right, #D94650, #DD9BA2)'
+						}
+					},
+					{
+						title: '家电维修',
+						desc: '家里变压器坏了，快来人啊！',
+						name: '林小姐',
+						dist: '3 km',
+						price: 1000,
+						thumb: 'https://raw.githubusercontent.com/Sanzro-Lee/HELPER_FACTION_official_weapp_customer/05f0a55cfcb143435e535c4f1d2ea7185029e060/src/material/%E5%88%86%E7%B1%BB%E6%A0%87%E7%A4%BA%E5%9B%BE-01.svg',
+						show: true,
+						statustagstyle: {
+							background: 'linear-gradient(to right, #5FD8A6, #A9DFA6)'
+						}
+					},
+					{
+						title: '其它',
+						desc: '清洁房子。',
+						name: '王先生',
+						dist: '2 km',
+						price: 500,
+						thumb: 'https://raw.githubusercontent.com/Sanzro-Lee/HELPER_FACTION_official_weapp_customer/05f0a55cfcb143435e535c4f1d2ea7185029e060/src/material/%E5%88%86%E7%B1%BB%E6%A0%87%E7%A4%BA%E5%9B%BE-04.svg',
+						show: true,
+						statustagstyle: {
+							background: 'linear-gradient(to right, #F8E255, #EBC354)'
+						}
+					},
+					{
+						title: '居家维修',
+						desc: '想把发霉的墙推倒，重新砌起来。',
+						name: '刘先生',
+						dist: '0.5 km',
+						price: 2000,
+						thumb: 'https://raw.githubusercontent.com/Sanzro-Lee/HELPER_FACTION_official_weapp_customer/05f0a55cfcb143435e535c4f1d2ea7185029e060/src/material/%E5%88%86%E7%B1%BB%E6%A0%87%E7%A4%BA%E5%9B%BE-02.svg',
+						show: true,
+						statustagstyle: {
+							background: 'linear-gradient(to right, #5FD8A6, #A9DFA6)'
+						}
+					},
+					{
+						title: '家电维修',
+						desc: '家里变压器坏了，快来人啊！',
+						name: '林小姐',
+						dist: '2.5 km',
+						price: 1000,
+						thumb: 'https://raw.githubusercontent.com/Sanzro-Lee/HELPER_FACTION_official_weapp_customer/05f0a55cfcb143435e535c4f1d2ea7185029e060/src/material/%E5%88%86%E7%B1%BB%E6%A0%87%E7%A4%BA%E5%9B%BE-01.svg',
+						show: true,
+						statustagstyle: {
+							background: 'linear-gradient(to right, #D94650, #DD9BA2)'
+						}
+					}
+				]
+			}
+		},
+		{
+			url: "http://helperfaction.com/message/list",
+			rule: {
+				"list": [
+					{
+						title: "俞师傅",
+						desc: "如果油漆换成蓝色会不会好点？",
+						time: "6 / 18 14:23",
+						count: "10"
+					},
+					{
+						title: "黄师傅",
+						desc: "那个，我的变压器已经买好了，请问您什么时候过来呢？",
+						time: "昨天 13 : 30",
+						count: "5"
+					},
+					{
+						title: "俞师傅",
+						desc: "如果油漆换成蓝色会不会好点？",
+						time: "昨天 6 : 24",
+						count: "100"
+					},
+					{
+						title: "张师傅",
+						desc: "那边的师傅说我的家具晚上6点才到，请问您有空吗？还是说明天？",
+						time: "9 : 30",
+						count: "8"
+					},
+					{
+						title: "刘师傅",
+						desc: "师傅！ 太感谢你啦！ 简直就是再世鲁班！",
+						time: "6 / 18 14:23",
+						count: "23"
+					},
+					{
+						title: "林师傅",
+						desc: "师傅手艺真好！",
+						time: "9 : 30",
+						count: "1"
+					},
+					{
+						title: "俞师傅",
+						desc: "如果油漆换成蓝色会不会好点？",
+						time: "6 / 18 14:23",
+						count: "0"
+					}
+				]
+			}
+		}
+])
