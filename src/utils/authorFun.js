7dde5102301eb4fb279ce4d79dd788cf837acb41
Sanzro-Lee <sanzrolee@gmail.com>
2020-10-05 00:59:32 +0800

    🐛 Bug(toptab.mpx index.mpx author.mpx authorfun.js): uI & author & skeleton & mpx.xfetch

--- src/utils/authorFun.js
+++ src/utils/authorFun.js
@@ -20,68 +20,50 @@ function authorCheck() {
   });
 }
 
-// 首页的 授权检测
-function indexAuthorCheck() {
-  wx.getSetting({
-    success: (res) => {
-      // 查看是否授权
-      if (res.authSetting["scope.userInfo"]) {
-        getUserInfo()
-      } else {
-        wx.navigateTo({
-          url: "author"
-        });
-      }
+// index 需要此方法获取用户所在区域
+function getUserDistrict(val) {
+  // 获取地理位置，精确到区
+  wx.getLocation({
+    type: "gcj02",
+    success(res) {
+      // 获取的信息后，设置首页用户所在的区域
+      qqmapsdk.reverseGeocoder({
+        location: {
+          latitude: res.latitude,
+          longitude: res.longitude
+        },
+        sig: "AY2W9KYUZtqHzoiEzLRs70lnqVrA3IWl",
+        success: (res) => {
+					let myDistrict = res.result.address_component.district
+					val.district = myDistrict
+        },
+        fail: (error) => {
+          // console.log(error)
+        }
+      })
+    },
+    fail: (error) => {
+      console.log("用户已拒绝");
+      Dialog.alert({
+        message: "你已拒绝开放定位功能，我们将无法为你服务。请点击右上角三个点的图标 - 设置 以开放定位功能，并重新进入此小程序，我们将更好地服务你！",
+        theme: "round-button",
+      }).then(() => {
+        // on confirm
+      });
     },
   });
 }
 
-// 此JS其它 function 需要调用的方法
+
+// mine 需要此方法跳转到 author 获取用户信息
 function getUserInfo() {
+  console.log("getUserInfo")
   // 已经授权，可以直接调用 getUserInfo 获取用户信息
   wx.getUserInfo({
     success: (res) => {
-      // 获取地理位置，精确到区
-      wx.getLocation({
-        type: "gcj02",
-        success(res) {
-          // 获取的信息后，设置首页用户所在的区域
-          qqmapsdk.reverseGeocoder({
-            location: {
-              latitude: res.latitude,
-              longitude: res.longitude
-            },
-            sig: "AY2W9KYUZtqHzoiEzLRs70lnqVrA3IWl",
-            success: (res) => {
-              let myDistrict = res.result.address_component.district
-              wx.setStorage({
-                key: "district",
-                data: myDistrict
-              })
-            },
-            fail: (error) => {
-              // console.log(error)
-            }
-          })
-          wx.switchTab({
-						url: "index",
-						// success: (res) => {
-						// 	let page = getCurrentPages().pop()
-						// 	if (page == undefined || page == null) return;
-						// 	page.onLoad();
-						// }
-          });
-        },
-        fail: (error) => {
-          console.log("用户已拒绝");
-          Dialog.alert({
-            message: "你已拒绝开放定位功能，我们将无法为你服务。请点击右上角三个点的图标 - 设置 以开放定位功能，我们将更好地服务你！",
-            theme: "round-button",
-          }).then(() => {
-            // on confirm
-          });
-        },
-      });
+			wx.switchTab({
+				url: "mine"
+			});
     },
     fail: (error) => {
       console.log("用户拒绝开放用户信息");
@@ -91,5 +73,6 @@ function getUserInfo() {
 
 export {
   authorCheck,
-  indexAuthorCheck
+	getUserDistrict,
+	getUserInfo
 }
